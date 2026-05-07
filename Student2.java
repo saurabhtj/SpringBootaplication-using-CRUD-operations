2564283519b9d6c088255e0ffa2f02a09ef5616f
saurabhtj <saurabhjijil@gmail.com>
2026-05-07 21:42:49 +0530

    Create Student2.java

---
+++ Student2.java
@@ -0,0 +1,32 @@
+package springbootweb1.project2;
+
+public class Student2 
+{
+    private int rollno;
+    private String name;
+    private String city;
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
